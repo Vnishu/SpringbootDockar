9fa28e87afe7c52377e4416eacfb1866740352e2
vishuvidyarthi <vishu.vidyarthi@gmail.com>
2021-08-19 12:27:15 +0100

    new branch

---
+++ src/test/java/com/example/springdockar/SpringDockarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
